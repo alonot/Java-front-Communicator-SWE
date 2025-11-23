c77072461d7ccd926a2f0cb2dca2ef4f701039f7
Sandeep Kumar <sandeepkumar75255@gmail.com>
2025-11-23 15:05:22 +0530

    added checkstyle

--- java/module-ux/src/main/java/com/swe/screenNVideo/SubscriberPacket.java
+++ java/module-ux/src/main/java/com/swe/screenNVideo/SubscriberPacket.java
@@ -1,19 +1,28 @@
 /**
  *  Contributed by Sandeep Kumar.
  */
+
 package com.swe.screenNVideo;
-
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 /**
  * Subscribe Packet.
+ * @param ip The IP address of the publisher.
+ * @param reqCompression True if compression is requested, false otherwise.
  */
 public record SubscriberPacket(String ip, boolean reqCompression) {
 
+    /**
+     * Byte value for true.
+     */
+    private static final byte BYTE_TRUE = 1;
+
+    /**
+     * Byte value for false.
+     */
+    private static final byte BYTE_FALSE = 0;
 
     /**
      * Serializes the string for networking layer.
@@ -22,15 +31,20 @@ public record SubscriberPacket(String ip, boolean reqCompression) {
     public byte[] serialize() {
         final int len = 4 * Integer.BYTES + 1; // 4 int for ip and one for boolean
         final ByteBuffer buffer = ByteBuffer.allocate(len + 1);
-        buffer.put((byte) (0)); // dummy to reuse a func in core
+        buffer.put((byte) 0); // dummy to reuse a func in core
 
-        int[] ipInts;
+        final int[] ipInts;
         ipInts = Arrays.stream(ip.split("\\.")).mapToInt(Integer::parseInt).toArray();
 
         for (int ipInt : ipInts) {
             buffer.putInt(ipInt);
         }
-        buffer.put((byte) (reqCompression ? 1 : 0));
+
+        if (reqCompression) {
+            buffer.put(BYTE_TRUE);
+        } else {
+            buffer.put(BYTE_FALSE);
+        }
 
         return buffer.array();
     }
